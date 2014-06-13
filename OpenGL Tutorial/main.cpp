af873e507be7e8eb70eb9d5b08c27bfbf5e67e86
PAKGI HYEOK <finelux@PAKGIui-MacBook-Pro.local>
2014-06-13 20:33:56 +0900

    Initial Commit

---
+++ OpenGL Tutorial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL Tutorial
+//
+//  Created by PAKGI HYEOK on 2014. 6. 13..
+//  Copyright (c) 2014년 anonymous. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
